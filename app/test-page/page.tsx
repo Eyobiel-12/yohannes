c26361d155e9af6d5ccd87a64984571529eeca48
Eyobiel-12 <Eyobielgoitom10@gmail.com>
2025-04-24 11:25:57 +0200

    Add simple test page

---
+++ app/test-page/page.tsx
@@ -0,0 +1,13 @@
+export default function TestPage() {
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-3xl font-bold mb-4">Test Page</h1>
+      <p className="mb-4">
+        This is a simple test page to verify that deployments are working correctly.
+      </p>
+      <p>
+        If you can see this page, the deployment is successful!
+      </p>
+    </div>
+  )
+} 
